324ba451711f7907a07c42e302e0c54ed6539fd0
juanpa <juan.paq72@mail.com>
2025-04-15 14:23:27 -0600

    decifrado y contadores

--- Juego.py
+++ Juego.py
@@ -2,19 +2,24 @@ class Juego:
     def __init__(self):
         self.jugando = False
         self.contador = 0
+        self.contador1 = 0
 
     def iniciar_juego(self):
         self.jugando = True
         print(f"\n¡Bienvenido al juego!")
         print("El juego ha comenzado..")
         self.cifrar()
+        self.descifrar()
         print("¡Gracias por jugar!\n")
     def iniciar_ordenado(self):
         self.jugando = True
         self.ordenamiento()
     def ordenamiento(self):
-        lista_num = [24,65,4,7,27,82,58,-200,-100,-50]
+        lista_num = input("Por favor, ingrese los números separados por espacios: ")
         lista_final = []
+        for i in lista_num.split():
+            lista_final.append(int(i))
+            
         while lista_num:
             elemento_maximo = lista_num[0]
             for numero in lista_num:
@@ -48,8 +53,35 @@ class Juego:
         print(f"Llevas {self.contador} palabras cifradas")
         print("")
 
-   
+    def descifrar(self):
+        mensaje_descifrado = ""
+        mensaje = input("Ingrese la palabra a descifrar: ")
+        alfabeto = 'abcdefghijklmnopqrstuvwxyz'
+        desplazamiento = 1
+        for caracter in mensaje.lower():
+            if caracter in alfabeto:
+                # Encontrar la posición del carácter en el alfabeto
+                posicion = alfabeto.find(caracter)
+                # Calcular la posición original antes del desplazamiento
+                posicion_original = (posicion - desplazamiento) % len(alfabeto)
+                # Añadir el carácter descifrado al mensaje
+                mensaje_descifrado += alfabeto[posicion_original]
+            else:
+                # Mantener caracteres que no están en el alfabeto
+                mensaje_descifrado += caracter
+        print("")
+        print("El mensaje descifrado es: ")
+        print(mensaje_descifrado)
+        self.contador1 += 1
+        print("")     
+        print("")
+        print(f"Llevas {self.contador1} palabras descifradas")
+        print("")   
+        return mensaje_descifrado 
 
+    def set_desplazamiento(self, nuevo_desplazamiento):
+        self.desplazamiento = nuevo_desplazamiento
+        return self.desplazamiento
     def salir(self):
         print(f"\nHasta luego. ¡Vuelve pronto!")
 
